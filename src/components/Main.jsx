b919ee78815a9a6146b78def84e92c0b9f22ad1e
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-24 11:03:18 -0400

    creates logout option and user auth foundation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
 // Main component renders login children and deals with logged in/logged out state
 
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import firebase from '../../firebase.config.js';
 
 const propTypes = {
@@ -9,12 +8,29 @@ const propTypes = {
 };
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+    };
+    this.signOut = this.signOut.bind(this);
+  }
+  signOut() {
+    firebase.auth()
+      .signOut()
+      .then(() => {
+        console.log('user signed out');
+      });
+  }
   render() {
     return (
       <div id="main-component">
         <h3>LAZR</h3>
         <div id="children-list">
           {this.props.children}
+        </div>
+        <div id="sign-out">
+          <a href="#" onClick={this.signOut}>Sign Out</a>
         </div>
       </div>
     );
--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,1 +1,15 @@
 // Will be the utility function that will check if user is logged in otherwise, it'll reroute them
+
+const React = require('react'),
+  firebase = require('../../firebase.config.js');
+
+function requireAuth(nextState, replace) {
+  if (firebase.auth().currentUser === null) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+}
+
+module.exports = requireAuth;
