e958441159908fc1b0e5bab3f027806b18f12b1d
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-24 13:14:56 -0400

    moves functionality in dashboard component to menu component

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,59 +3,15 @@ import request from 'superagent';
 import { withRouter } from 'react-router';
 import Date from './Date.jsx';
 import Menu from './Menu.jsx';
-import Active from './active/Active.jsx';
-import Snooze from './snoozed/Snooze.jsx';
-import Archive from './archive/Archive.jsx';
 
 class Dashboard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      active: true,
-      snooze: false,
-      archive: false,
-    };
-    this.activeHandler = this.activeHandler.bind(this);
-    this.snoozeHandler = this.snoozeHandler.bind(this);
-    this.archiveHandler = this.archiveHandler.bind(this);
-  }
-  activeHandler() {
-    this.setState({
-      active: true,
-      snooze: false,
-      archive: false,
-    });
-    console.log(this.state.active)
-  }
-  snoozeHandler() {
-    this.setState({
-      snooze: true,
-      active: false,
-      archive: false,
-    });
-    console.log(this.state.snooze)
-  }
-  archiveHandler() {
-    this.setState({
-      archive: true,
-      snooze: false,
-      active: false,
-    });
-    console.log(this.state.archive)
-  }
+
   render() {
     return (
       <div id="dashboard">
         <h2> DASHBOARD </h2>
         <Date />
-        <Menu
-          activeHandler={this.activeHandler}
-          snoozeHandler={this.snoozeHandler}
-          archiveHandler={this.archiveHandler}
-        />
-        {this.state.active ? <Active activeHandler={this.activeHandler} /> : false }
-        {this.state.snooze ? <Snooze snoozeHandler={this.snoozeHandler} /> : false }
-        {this.state.archive ? <Archive archiveHandler={this.archiveHandler} /> : false }
+        <Menu />
         {/* <Filters /> */}
         {/* <Logout /> */}
       </div>
--- src/components/dashboard/Menu.jsx
+++ src/components/dashboard/Menu.jsx
@@ -1,43 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router';
+import Active from './active/Active.jsx';
+import Snooze from './snoozed/Snooze.jsx';
+import Archive from './archive/Archive.jsx';
 
-// const propTypes = {
-//   activeHandler: React.PropTypes.function,
-//   snoozeHandler: React.PropTypes.function,
-//   archiveHandler: React.PropTypes.function,
-// };
-
-const Menu = ({ activeHandler,
-                snoozeHandler,
-                archiveHandler,
-              }) => {
-  return (
-    <div id="menu-icons">
-      <Link to="/:active" onClick={activeHandler}>
-        <img
-          id="home-icon"
-          className="icons"
-          role="presentation"
-        />
-      </Link>
-      <Link to="/:snooze" onClick={snoozeHandler}>
-        <img
-          id="snooze-icon"
-          className="icons"
-          role="presentation"
-        />
-      </Link>
-      <Link to="/:archive" onClick={archiveHandler}>
-        <img
-          id="archive-icon"
-          className="icons"
-          role="presentation"
-        />
-      </Link>
-    </div>
-  );
-};
-
-// Menu.propTypes = propTypes;
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: true,
+      snooze: false,
+      archive: false,
+    };
+    this.activeHandler = this.activeHandler.bind(this);
+    this.snoozeHandler = this.snoozeHandler.bind(this);
+    this.archiveHandler = this.archiveHandler.bind(this);
+  }
+  activeHandler() {
+    this.setState({
+      active: true,
+      snooze: false,
+      archive: false,
+    });
+  }
+  snoozeHandler() {
+    this.setState({
+      snooze: true,
+      active: false,
+      archive: false,
+    });
+  }
+  archiveHandler() {
+    this.setState({
+      archive: true,
+      snooze: false,
+      active: false,
+    });
+  }
+  render() {
+    return (
+      <div id="menu-icons">
+        <Link to="/:active" onClick={this.activeHandler}>
+          <img
+            id="home-icon"
+            className="icons"
+            role="presentation"
+          />
+        </Link>
+        <Link to="/:snooze" onClick={this.snoozeHandler}>
+          <img
+            id="snooze-icon"
+            className="icons"
+            role="presentation"
+          />
+        </Link>
+        <Link to="/:archive" onClick={this.archiveHandler}>
+          <img
+            id="archive-icon"
+            className="icons"
+            role="presentation"
+          />
+        </Link>
+        {this.state.active ? <Active activeHandler={this.activeHandler} /> : false }
+        {this.state.snooze ? <Snooze snoozeHandler={this.snoozeHandler} /> : false }
+        {this.state.archive ? <Archive archiveHandler={this.archiveHandler} /> : false }
+      </div>
+    );
+  }
+}
 
 export default withRouter(Menu);
