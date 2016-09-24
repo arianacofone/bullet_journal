edaea3ea26858d18a770b3cf1fb8784c54913849
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-24 16:58:40 -0400

    sets up http get and post request through active and input components

--- src/components/dashboard/active/Active.jsx
+++ src/components/dashboard/active/Active.jsx
@@ -1,17 +1,58 @@
-// This document will render the active list components
-import React from 'react';
+import React, { Component } from 'react';
+import request from 'superagent';
 import { withRouter } from 'react-router';
+import firebase from '../../../../firebase.config.js';
 import Input from './Input.jsx';
-import ToDoList from './ToDoList.jsx';
+// import ToDoList from 'ToDoList.jsx';
 
-const Active = () => {
-  return (
-    <div>
-      <h3>ACTIVE</h3>
-      <Input />
-      <ToDoList />
-    </div>
-    );
-};
+class Active extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+    this.httpPost = this.httpPost.bind(this);
+  }
+  componentDidMount() {
+    this.httpGet();
+  }
+  httpGet() {
+    const userId = firebase.auth().currentUser.uid;
+    const url = `https://lazr-1da5a.firebaseio.com/users/${userId}/items.json`;
+    request.get(url)
+           .then((response) => {
+             const itemsData = response.body;
+             let items = [];
+             if (itemsData) {
+               items = Object.keys(itemsData).map((id) => {
+                 const todoItem = itemsData[id];
+                 return {
+                   id,
+                   localInput: todoItem.localInput,
+                 };
+               });
+             }
+             this.setState({ items });
+           });
+  }
+  httpPost({ localInput }) {
+    const userId = firebase.auth().currentUser.uid;
+    const url = `https://lazr-1da5a.firebaseio.com/users/${userId}/items.json`;
+    request.post(url)
+           .send({ localInput })
+           .then(() => {
+             this.httpGet();
+           });
+  }
+  render() {
+    return (
+      <div>
+        <h3>ACTIVE</h3>
+        <Input handlePost={this.httpPost} />
+        {/* <ToDoList /> */}
+      </div>
+      );
+  }
+}
 
 export default withRouter(Active);
--- src/components/dashboard/active/Input.jsx
+++ src/components/dashboard/active/Input.jsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Input = () => {
-  return (
-    <div>
-      <h3>Input Form</h3>
-      <form className="post-display">
-        <input
-          type="text"
-          name="todo"
-        />
-        <input
-          type="submit"
-          value="DOT"
-        />
-        <input
-          type="submit"
-          value="CIRCLE"
-        />
-        <input
-          type="submit"
-          value="DASH"
-        />
-      </form>
-    </div>
-    );
+const propTypes = {
+  handlePost: React.PropTypes.func,
 };
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      localInput: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEditInput = this.handleEditInput.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handlePost({
+      localInput: this.state.localInput,
+    });
+  }
+  handleEditInput(e) {
+    const data = e.target.value;
+    this.setState({ localInput: data })
+  }
+  render() {
+    return (
+      <div id="input">
+        <h3>Input Form</h3>
+        <form className="input-display" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="todo"
+            value={this.state.localInput}
+            onChange={this.handleEditInput}
+          />
+          <input
+            type="submit"
+            value="DOT"
+          />
+          <input
+            type="submit"
+            value="CIRCLE"
+          />
+          <input
+            type="submit"
+            value="DASH"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+Input.propTypes = propTypes;
 
 export default Input;
