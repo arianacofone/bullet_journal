28f8826c8f89d4cc5a8b517107cd1b9860d786b6
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-25 18:15:07 -0400

    sets get request to database

--- src/components/dashboard/active/ToDoList.jsx
+++ src/components/dashboard/active/ToDoList.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Input from './Input.jsx';
 
-const ToDoList = () => {
-  return (
-    <div>
-      <h3>To Do List</h3>
-      <div>Items Go Here</div>
-    </div>
+const ToDoList = (props) => {
+  const ids = Object.keys(props.items);
+  const itemElements = ids.map((id, idx) => {
+    const item = props.items[id];
+    return (
+      <li key={idx}>
+        <form>
+          <input value={item.localInput} />
+          <input
+            name="task"
+            type="submit"
+            value="Done"
+          />
+          <input
+            name="event"
+            type="submit"
+            value="Snooze"
+          />
+          <input
+            name="note"
+            type="submit"
+            value="Delete"
+            // onSubmit={this.props.httpPost}
+          />
+        </form>
+      </li>
     );
+  });
+  return (
+    <ul>
+      {itemElements}
+    </ul>
+  );
 };
+
+// class ToDoList extends Component {
+//   render() {
+//     const itemElements = props.items.map((item, idx) => {
+//       return (
+//         <li key={idx}>
+//           <Input
+//             item={item.localInput}
+//           />
+//         </li>
+//       );
+//     });
+//     return (
+//       <ul>
+//         {itemElements}
+//       </ul>
+//     );
+//   }
+// }
 
 export default ToDoList;
