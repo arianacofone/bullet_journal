ec7d29be49f538b6619b3d43eb2be2a1e4f7b6f9
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-25 18:11:30 -0400

    centers all database requests from dashboardcomponent

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
-import request from 'superagent';
 import { withRouter } from 'react-router';
+import firebase from '../../../firebase.config.js';
 import Date from './Date.jsx';
 import Menu from './Menu.jsx';
 
 class Dashboard extends Component {
-
+  constructor() {
+    super();
+    // this.state = {
+    //   loggedIn: false,
+    // };
+    this.signOut = this.signOut.bind(this);
+  }
+  signOut() {
+    firebase.auth()
+      .signOut()
+      .then(() => {
+        console.log('user signed out');
+      });
+  }
   render() {
     return (
       <div id="dashboard">
         <h2> DASHBOARD </h2>
         <Date />
         <Menu />
-        {/* <Filters /> */}
-        {/* <Logout /> */}
+        <button id="sign-out" onClick={this.signOut}> logout</button>
       </div>
     );
   }
-}
+};
+
 
 export default withRouter(Dashboard);
