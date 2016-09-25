298e3ae2010f8a0c981d3e2a2fd47c4dec786f4d
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-25 18:08:53 -0400

    deletes app component and removes from routes

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -3,7 +3,6 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Main from '../components/Main.jsx';
 import Login from '../components/Login.jsx';
 import Register from '../components/Register.jsx';
-import App from '../components/App.jsx';
 import requireAuth from '../utils/Auth.js';
 import Dashboard from '../components/dashboard/Dashboard.jsx';
 import Active from '../components/dashboard/active/Active.jsx';
@@ -16,8 +15,7 @@ const Routes = () => {
       <Route path="/" component={Main}>
         <IndexRoute component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/:userId" component={App} onEnter={requireAuth}>
-          <IndexRoute component={Dashboard} />
+        <Route path="/:userId" component={Dashboard} onEnter={requireAuth}>
           <Route path="/:active" component={Active} />
           <Route path="/:snooze" component={Snooze} />
           <Route path="/:archive" component={Archive} />}
