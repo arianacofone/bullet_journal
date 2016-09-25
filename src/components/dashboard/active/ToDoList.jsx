f5ed1b6e9c565d51f567371366bb83524fca8e6c
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-25 18:20:36 -0400

    changes todolist from pure function to class

--- src/components/dashboard/active/ToDoList.jsx
+++ src/components/dashboard/active/ToDoList.jsx
@@ -1,58 +1,44 @@
 import React, { Component } from 'react';
 import Input from './Input.jsx';
 
-const ToDoList = (props) => {
-  const ids = Object.keys(props.items);
-  const itemElements = ids.map((id, idx) => {
-    const item = props.items[id];
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const ids = Object.keys(this.props.items);
+    const itemElements = ids.map((id, idx) => {
+      const item = this.props.items[id];
+      return (
+        <li key={idx}>
+          <form>
+            <input value={item.localInput} />
+            <input
+              name="task"
+              type="submit"
+              value="Done"
+            />
+            <input
+              name="event"
+              type="submit"
+              value="Snooze"
+            />
+            <input
+              name="note"
+              type="submit"
+              value="Delete"
+              // onSubmit={this.props.httpPost}
+            />
+          </form>
+        </li>
+      );
+    });
     return (
-      <li key={idx}>
-        <form>
-          <input value={item.localInput} />
-          <input
-            name="task"
-            type="submit"
-            value="Done"
-          />
-          <input
-            name="event"
-            type="submit"
-            value="Snooze"
-          />
-          <input
-            name="note"
-            type="submit"
-            value="Delete"
-            // onSubmit={this.props.httpPost}
-          />
-        </form>
-      </li>
+      <ul>
+        {itemElements}
+      </ul>
     );
-  });
-  return (
-    <ul>
-      {itemElements}
-    </ul>
-  );
-};
-
-// class ToDoList extends Component {
-//   render() {
-//     const itemElements = props.items.map((item, idx) => {
-//       return (
-//         <li key={idx}>
-//           <Input
-//             item={item.localInput}
-//           />
-//         </li>
-//       );
-//     });
-//     return (
-//       <ul>
-//         {itemElements}
-//       </ul>
-//     );
-//   }
-// }
+  }
+}
 
 export default ToDoList;
