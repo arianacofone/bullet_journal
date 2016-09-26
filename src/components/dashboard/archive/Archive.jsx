ae1d41a0a2ac9c59e77f2ac931a3316cd3974e1b
arianadziedzic <ariana.dziedzic@gmail.com>
2016-09-26 17:14:11 -0400

    creates placeholder page for future react component

--- src/components/dashboard/archive/Archive.jsx
+++ src/components/dashboard/archive/Archive.jsx
@@ -5,8 +5,28 @@ import { withRouter } from 'react-router';
 
 const Archive = () => {
   return (
-    <div>
-      <h4>Archive is rendering</h4>
+    <div id="archive-list">
+      <h3>ARCHIVE</h3>
+      <p className="day">FRIDAY SEPTEMBER 23RD</p>
+      <div className="archive-item">
+        <p> * Pay Rent</p>
+      </div>
+      <div className="archive-item">
+        <p> o Schedule Dentist Appointment</p>
+      </div>
+      <div className="archive-item">
+        <p> - Should we get a dog?</p>
+      </div>
+      <div className="archive-item">
+        <p> * Clean Bathroom </p>
+      </div>
+      <p className="day">THURSDAY SEPTEMBER 22ND</p>
+      <div className="archive-item">
+        <p> - Do fish fart? </p>
+      </div>
+      <div className="archive-item">
+        <p> o Throw out garbage </p>
+      </div>
     </div>
   );
 };
